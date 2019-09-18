71de6327a87b3bb280eff1db95d156ce61f2a97c
Alexandergrxnt <alexandergrxnt@gmail.com>
2019-09-18 21:50:27 +0100

    added bouncing loader page

---
+++ nav.js
@@ -0,0 +1,9 @@
+function openNav(){
+    document.getElementById("overlayNav").style.width ="150px";
+    document.getElementById("blankPanel").style.width ="100%";
+}
+
+function closeNav(){
+    document.getElementById("overlayNav").style.width = "0%";
+    document.getElementById("blankPanel").style.width ="0%";
+}
