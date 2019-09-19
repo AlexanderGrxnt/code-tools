68bd025fa845a2556c541932a32fa5548a819b60
Alexandergrxnt <alexandergrxnt@gmail.com>
2019-09-19 09:52:50 +0100

    Updated header font

--- nav.js
+++ nav.js
@@ -7,3 +7,9 @@ function closeNav(){
     document.getElementById("overlayNav").style.width = "0%";
     document.getElementById("blankPanel").style.width ="0%";
 }
+
+// (function openNavBar(){
+//     document.getElementById("navBar").style.height ="100vh";
+//     console.log("navbar");
+// })();
+
