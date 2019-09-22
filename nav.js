28ff716f25faf6dac9a6f9260f87a5dc5a240c90
Alexandergrxnt <alexandergrxnt@gmail.com>
2019-09-22 13:47:18 +0100

    Added cursors and icons

--- nav.js
+++ nav.js
@@ -1,12 +1,32 @@
-function openNav(){
-    document.getElementById("overlayNav").style.width ="150px";
+//HTML
+
+function openHTMLNav(){
+    document.getElementById("overlayNav-HTML").style.width ="150px";
     document.getElementById("blankPanel").style.width ="100%";
+    document.getElementById("overlayNav-CSS").style.width = "0%";
 }
 
+
+//CSS
+
+function openCSSNav(){
+    document.getElementById("overlayNav-CSS").style.width ="150px";
+    document.getElementById("blankPanel").style.width ="100%";
+    document.getElementById("overlayNav-HTML").style.width ="0%";
+}
+
+//CLOSE
+
 function closeNav(){
-    document.getElementById("overlayNav").style.width = "0%";
+    document.getElementById("overlayNav-HTML").style.width ="0%";
+    document.getElementById("overlayNav-CSS").style.width = "0%";   
     document.getElementById("blankPanel").style.width ="0%";
 }
+
+
+
+
+
 
 // (function openNavBar(){
 //     document.getElementById("navBar").style.height ="100vh";
