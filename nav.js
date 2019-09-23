fd2a5ab74615f1f8d85830e74209e413f183a39b
Alexandergrxnt <alexandergrxnt@gmail.com>
2019-09-23 17:42:34 +0100

    added basic HTML page

--- nav.js
+++ nav.js
@@ -1,7 +1,7 @@
 //HTML
 
 function openHTMLNav(){
-    document.getElementById("overlayNav-HTML").style.width ="150px";
+    document.getElementById("overlayNav-HTML").style.width ="180px";
     document.getElementById("blankPanel").style.width ="100%";
     document.getElementById("overlayNav-CSS").style.width = "0%";
 }
